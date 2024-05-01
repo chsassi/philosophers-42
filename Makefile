0fbe455bb598f9f1cfb7b329fe6966650c6feca3
Christian Sassi <chsassi@c2r5p12.42firenze.it>
2024-05-01 19:21:47 +0200

    -

---
+++ Makefile
@@ -0,0 +1,33 @@
+NAME = philo
+
+CC = cc
+CFLAGS = -Wextra -Werror -Wall -g
+COMPILE = $(CC) $(CFLAGS) -c
+ARCHIVE = ar rc $(NAME)
+
+GREEN=\033[0;32m
+RED=\033[0;31m
+BLUE=\033[0;34m
+YELLOW=\033[0;33m
+RESET=\033[0m
+
+SRC =	./utils.c \
+		./main.c
+
+OBJ = $(SRC:.c=.o)
+
+all: $(NAME)
+
+$(NAME): $(OBJ)
+	$(CC) $(CFLAGS) -I. $(OBJ) -o $(NAME)
+	@echo "$(GREEN)	PHILOSOPHERS COMPILED! ✅$(RESET)"
+
+RM = rm -f
+
+clean:
+	$(RM) $(OBJ)
+
+fclean: clean
+	$(RM) $(NAME)
+
+re: fclean all
---
+++ philo.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philos.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chsassi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/01 18:45:22 by chsassi           #+#    #+#             */
+/*   Updated: 2024/05/01 18:45:23 by chsassi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <pthread.h>
+# include <sys/time.h>
+
+typedef struct	s_philo
+{
+	
+}	t_philo
+
+
+#endif
